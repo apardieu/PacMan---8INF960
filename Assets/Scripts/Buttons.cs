d9e603b8c672cf649151faaeb1532a84a25fc1cc
Agathe21 <agathecagin2009@gmail.com>
2019-09-06 18:15:23 -0400

    Son

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+//using UnityEngine.Component;
+
+public class Buttons : MonoBehaviour
+{
+    public enum ButtonTypes{Play, Pause, Stop, Mute}
+
+    public ButtonTypes type;
+    public Texture mute;
+    public Texture unmute;
+
+    private SoundControl sound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sound=Camera.main.GetComponent<SoundControl>();
+    }
+
+    void OnMouseDown()
+	{
+        if(sound==null) return;
+        switch (type)
+        {
+            case ButtonTypes.Play:
+                sound.Play();
+                break;
+            case ButtonTypes.Pause:
+                sound.Pause();
+                break;
+            case ButtonTypes.Stop:
+                sound.Stop();
+                break;
+            case ButtonTypes.Mute:
+                sound.ToggleMute();
+				//if(sound.IsMute()){
+					//if(mute!=null)
+						//renderer.material.mainTexture=mute;
+				//} else {
+					//if(unmute!=null)
+							//renderer.material.mainTexture=unmute;
+				//}                
+			break;
+        }
+    }
+}
---
+++ Assets/Scripts/SoundControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+//using UnityEngine.Component;
+
+public class SoundControl : MonoBehaviour
+{
+    public AudioClip ambient;
+    public Texture mute;
+    public Texture unmute;
+
+    public void Play()
+    {
+        GetComponent<AudioSource>().Play();
+    }
+
+    public void Stop()
+    {
+        GetComponent<AudioSource>().Stop();
+    }
+
+    public void Pause()
+    {
+        GetComponent<AudioSource>().Pause();
+    }
+
+    public void ToggleMute()
+    {
+        if(GetComponent<AudioSource>().mute){
+            GetComponent<AudioSource>().mute=false;
+        } else {
+            GetComponent<AudioSource>().mute=true;
+        }
+    }
+
+    //public bool IsMute(){
+        //return audio.mute;
+    //}
+}
